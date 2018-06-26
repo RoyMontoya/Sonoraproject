1ee25296aa3391ac45cc3b03ecb3a9da0adfda95
Roy Montoya <roymontoya89@gmail.com>
2018-06-26 11:18:09 -0700

    init

---
+++ src/components/NavBar.js
@@ -0,0 +1,37 @@
+import React, {Component} from 'react';
+
+class NavBar extends React.Component {
+  render(){
+    return (
+      <div>
+        <nav className="navbar navbar-expand navbar-dark bg-dark navbar-default">
+          <a className="navbar-brand" href="#">Nutricion Sonora</a>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample02" aria-controls="navbarsExample02" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          <div className="collapse navbar-collapse" >
+            <ul className="nav navbar-nav ml-auto">
+              <li className="nav-item active">
+                <a className="nav-link" href="#">Inicio
+                  <span className="sr-only">(current)</span>
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Nutricion</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Instructores</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">About</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+    );
+  }
+}
+
+export default NavBar;
