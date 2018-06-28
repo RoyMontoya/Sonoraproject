03306450400579ef9f03ad299b8333aad9e5f086
Roy Montoya <roymontoya89@gmail.com>
2018-06-27 17:44:33 -0700

    started about section

---
+++ src/components/About.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react';
+import '../styles/About.css';
+import banner from '../img/about-background.jpeg';
+import separator from '../img/healthy.jpg';
+
+class About extends React.Component {
+
+  render() {
+    return (
+      <div>
+      <div className="container">
+        <div className="row">
+          <img className="about-img" src={banner} alt="healthy-Banner" title="banner"/>
+          <div className="col-md-12 about-body"></div>
+          <h2 className="over-image-title">¿Quienes somos?</h2>
+          <p className="over-image-text">Somos personas como tu que buscamos generar un cambio en la comunidad, promiviendo estilos de vida saludables
+          y proporcionando las herramientas para que cualquier persona pueda adquirir un estilo de vida saludable.<br/><br/>
+          Creemos que vivir al máximo es la unica forma de vivir y que solo podemos lograrlo a traves de estilos de vida saludables como los que encontraras en esta pagina.</p>
+          <div className="image-wrapper"><img className="separator" src={separator}/></div>
+
+          <h2></h2>
+
+
+        </div>
+      </div>
+
+    </div>);
+  }
+}
+
+export default About;
--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import NavBar from './Navbar'
 import Banner from './Banner'
 import NewsFeed from './NewsFeed'
 import Footer from './Footer'
+import About from './About'
 import '../styles/App.css';
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faCheckSquare, faCoffee } from '@fortawesome/free-solid-svg-icons'
@@ -15,6 +16,7 @@ class App extends Component {
     return (<div className="App">
       <Banner/>
       <NavBar/>
+      {/* <About/> */}
       <NewsFeed/>
       <Footer/>
 
